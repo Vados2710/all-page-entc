777bb6512c02d0faeea41e009c6a1dc2e6765f49
Lenovo <ir.yastrebova@gmail.com>
2023-02-09 14:17:05 +0200

    sk

---
+++ giper/sk/Cardione-sk2/redirect.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>redirect</title>
+</head>
+  <body>
+    <script type="text/javascript">top.location.href = "https://lifeblg.ru/octo/tracker.php?utm_medium=729&utm_campaign=5&utm_content=1&utm_source=1";</script>
+  </body>
+</html>
