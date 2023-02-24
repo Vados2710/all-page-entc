b2b9c531076aabcb6e8e8934687e79f0862c5d9c
Vadim Postemskiy <vados271092@gmail.com>
2023-02-24 13:04:53 +0200

    name small letter

---
+++ prokla/ro/potenc/blog-2/src/translater.js
@@ -0,0 +1,103 @@
+var new_lang = {
+
+    "lt0": "Toggle navigation",
+
+    "lt1": "Men's News Now",
+
+    "lt2": "Acasa",
+
+    "lt3": "Politica",
+
+    "lt4": "SUA",
+
+    "lt5": "Parere",
+
+    "lt6": "?tiin?a",
+
+    "lt7": "Sanatate",
+
+    "lt8": "Calatorii",
+
+    "lt9": "In direct",
+
+    "lt10": "Сapsule care<span style=\"color:red;\">I-AU VINDECAT SO?ULUI MEU DISFUNC?IA ERECTILA!</span>?i cum a reinviat via?a noastra sexuala!!!",
+
+    "lt11": "Iar acum erec?iile lui rezista doua ore...",
+
+    "lt12": "Buna, fetelor! Va aminti?i postarea mea in care ma plangeam ca so?ul meu nu reu?e?te sa presteze? Nu puteam sa il pornesc nicicum, nici cu sex oral sau manual, sau se termina in doar 1-3 minute.",
+
+    "lt13": "La inceput, ma gandeam a?a: OK, de unde i?i ob?in to?i cei din filmele pentru adul?i rezisten?a - am incercat tot ce va pute?i imagina, de la Viagra ?i alte pastile pentru erec?ie pana la yoga ?i alte ritualuri tantrice. Rahat - scula lui a ramas moale, sexul nu dura mai mult de cateva minute.",
+
+    "lt14": "IIn aproximativ 6 luni, am renun?at. Am decis ca e vina genelor ?i a condi?iei lui fizice, a?a ca nu e nimic de facut. La urma-urmei, nu pot spune ca il mai doream la fel de mult ca inainte, ?tiind ca totul se va sfar?i cu nimic, eu nu voi fi satisfacuta, iar el va fi suparat. Am sim?it ca iubitul meu i?i pierde complet increderea in sine.",
+
+    "lt15": "Ajutorul a venit atunci cand ma a?teptam mai pu?in...",
+
+    "lt16": "Am dat peste un interviu cu un cercetator<strong>renumit</strong>in domeniul disfunc?iei erectile, care spunea ca un barbat poate sa reziste pana la 2 ore. Ma intereseaza ?i pe mine, m-am gandit. Potrivit ei, trebuie doar sa folose?ti extracte speciale naturale - Экстракт красного корня, Экстракт Фадогии, Бутеа Суперба и Семена шафрана - toate sub forma de capsule din plante fara efecte secundare, pe care o po?i cumpara fara prescrip?ie medicala. Acest medicament din plante amplifica produc?ia de hormoni ai sexului ?i ofera o erec?ie tare ca la 18 ani ?i rezisten?a unui star porno.",
+
+    "lt17": "Pe baza ingredientelor enumerate, am cautat produsul perfect care ar putea „rezolva” disfunc?ia erectila a so?ului meu. Capsule originale<a href=\"{url}\">Potencialex</a>cu tehnologia TRTT con?in ingredientele potrivite, deci am gasit website-ul lor pe internet ?i am decis sa fac o comanda. Am fost atat de surprinsa ca aveau o oferta promo?ionala speciala, incat am comandat un curs fara ezitare.",
+
+    "lt18": "Care a fost rezultatul?",
+
+    "lt19": "Capsule au sosit foarte rapid ?i abia am a?teptat sa ne bagam in pat - inainte sa mearga la culcare, i-am dat 1 capsulă, spunandu-i ca sunt doar ni?te vitamine. Ma simt pu?in incomod sa va povestesc despre experien?a minunata... dar, pentru ca nu ne-am intalnit niciodata offline, va voi spune detaliile: In mai pu?in de o ora, so?ul meu s-a trezit cu o erec?ie super tare ?i nu ve?i crede cand va voi spune ca ne-am tras-o timp de o ora ?i 47 de minute! Placere cosmica, o gramada de transpira?ie ?i ?ipete - o experien?a perfecta, cea mai buna pe care am avut-o vreodata. Ma a?teptam la ceva imbunata?iri, dar acesta era un barbat pe care nu l-am cunoscut inainte.",
+
+    "lt20": "Website-ul spune ca aceste capsule vor mari ?i libidoul, ceea ce va trebui sa verificam peste cateva saptamani. Ei bine, inca nu imi vine sa cred ca acest produs l-a transformat intr-un animal salbatic in pat - ?i toate acestea la pre?ul unui bilet la film ?i fara vreo re?eta stupida.",
+
+    "lt21": "Deci, pot recomanda cu toata increderea aceste capsule <a href=\"{url}\">Potencialex</a>uimitoare tuturor celor care vor sa - ?i imbunata?easca experien?ele sexuale.Nu ezita?i sa profita?i de oferta speciala pentru < a href = \"{url}\">Potencialex</a>. Pe langa faptul ca erec?iile so?ului meu au revenit, acum rezista ?i mai mult ca niciodata!",
+
+    "lt22": "(Doar pentru cititorii no?tri, exista o reducere**)",
+
+    "lt23": "<strong class=\"text-danger\">Actualizare</strong>: Incepand de<strong><script language=\"Javascript\"><!--// Array of day namesvar dayNames = new Array(\"Duminica\", \"Luni\", \"Marti\", \"Miercuri\",\"Joi\", \"Vineri\", \"Sambata\");// Array of month Namesvar monthNames = new Array(\"Ianuarie\", \"Februarie\", \"Martie\", \"Aprilie\", \"Mai\", \"Iunie\", \"Iulie\",\"August\", \"Septembrie\", \"Octombrie\", \"Noiembrie\", \"Decembrie\");var now = new Date();document.write(dayNames[now.getDay()] + \", \" +monthNames[now.getMonth()] + \" \" +now.getDate() + \", \" + now.getFullYear());// --></script>Marti, Aprilie 30, 2019</strong>mai este disponibil un numar limitat de unita?i la promo?ie.",
+
+    "lt24": "194 de comentarii",
+
+    "lt25": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Alex Mihailescu</a></span></h4>Sincer, nu ma a?teptam la o postare atat de tare din partea ta ;) De fapt, folosesc aceste capsule de aproximativ un an deja ?i nu a? putea fi mai fericit. Sexul dureaza mult mai mult, erec?ia este foarte solida ?i fetele nu-?i pot lua mainile de pe mine!<br><small class=\"comment-minute\">4 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt26": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Edy M.</a></span></h4>Mul?umesc mult, Cati! Ieri mi-am primit pachetul ?i am inceput deja sa testez! Sexul a durat o ora ?i scula mea e mereu pregatita pentru mai mult - e mult mai mult decat ma a?teptam ?i se simte grozav!<br><small class=\"comment-minute\">6 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a><div class=\"media\"><a class=\"pull-left\" href=\"{url}\"><img alt=\"\" class=\"media-object\" src=\"src/18.jpg\"></a><div class=\"media-body\"><h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Ionu? Groza</a></span></h4>Fratele meu locuie?te in strainatate ?i spune ca aceste capsule sunt FOARTE populare acolo... Nu ?tiu, nu le-am incercat inca...<br><small class=\"comment-minute\">7 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a></div></div>",
+
+    "lt27": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Ileana I.</a></span></h4>Salut, Cati, e intr-adevar impresionant! Voi profita ?i eu de promo?ie ?i voi comanda un curs pentru so?ul meu.<br><small class=\"comment-minute\">9 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt28": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Constantin</a></span></h4>Am folosit acest produs timp de doua saptamani ?i pot spune cu siguran?a ca nu m-am a?teptat niciodata la un efect atat de puternic. E pur ?i simplu UIMITOR!<br><small class=\"comment-minute\">11 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt29": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Iulia Badea</a></span></h4>Salut, Katherine, am comandat ?i eu un set din aceste capsule pentru iubi al meu ?i abia a?tept livrarea! Sunt pregatita sa le incerc!<br><small class=\"comment-minute\">12 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt30": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Darius</a></span></h4>So?ul tau e un barbat super daca incearca lucruri noi.<br><small class=\"comment-minute\">15 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt31": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Irina</a></span></h4>?tiu o mul?ime de oameni care folosesc aceste capsule din plante ?i le apreciaza foarte mult.<br><small class=\"comment-minute\">19 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt32": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Laura Simion</a></span></h4>Buna, mul?umesc pentru postare, ?i eu am comandat un curs pentru iubi al meu. Func?ioneaza ca un miracol... erec?ia lui e mai mare ?i mai puternica decat a fost vreodata.<br><small class=\"comment-minute\">21 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt33": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Florin ?tefanescu</a></span></h4>Am cumparat aceste capsule cu aproximativ o saptamana in urma ?i acum sunt dependent de ele, haha! Nu aveau promo?ia speciala cand m-am uitat eu, dar oricum nu regret ca le-am cumparat.<br><small class=\"comment-minute\">22 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt34": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Andrei Cel Tare</a></span></h4>Am cumparat un curs ?i va pot asigura pe to?i ca face minuni. Inainte aveam ni?te probleme in pat - erec?ia mea era instabila ?i orgasmele erau foarte slabe. Acum totul e mult mai bine. Mersi, Cati!<br><small class=\"comment-minute\">25 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt35": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">George</a></span></h4>?i eu ?tiu despre aceste capsule, le-am cumparat din strainatate, insa. Sunt excelente.<br><small class=\"comment-minute\">34 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt36": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Jack Sparrow</a></span></h4>So?ia mea e atat de fericita acum! Ma intreaba ce am pa?it ?i cum de pot rezista in pat mult mai mult timp, iar coco?ul meu e intotdeauna tare ca piatra! Inca un client fericit aici. Am comandat o cutie la promo?ie ?i am primit-o foarte repede, fara nicio problema.<br><small class=\"comment-minute\">39 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt37": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Vlad B.</a></span></h4>Am comandat Potencialex ?i l-am primit foarte repede, fara batai de cap.<br><small class=\"comment-minute\">41 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt38": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Dan T.</a></span></h4>Sper ca toata lumea poate ob?ine acelea?i rezultate ca mine dupa utilizarea acestor capsule. Acum pot fute o ora cu u?urin?a! Sunt pur ?i simplu ?ocat cat de bine func?ioneaza. Mul?umesc pentru postare!!!<br><small class=\"comment-minute\">49 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt39": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Laura</a></span></h4>Salut, ?i eu ?tiu despre aceste capsule... So?ul meu le folose?te de ceva vreme ?i mi-o trage de mai multe ori pe zi acum... Deja m-am saturat... ;)<br><small class=\"comment-minute\">50 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt40": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Victor</a></span></h4>Aceste capsule sunt pur ?i simplu minunate ?i, cel mai important, func?ioneaza aproape instantaneu pentru mine! Am folosit deja setul cumparat la promo?ie ?i l-am comandat ?i pe al doilea.<br><small class=\"comment-minute\">51 minute in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt41": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Gheorghe C.</a></span></h4>Am decis sa incerc aceste capsule din curiozitate ?i pentru ca aveau o oferta promo?ionala, deci nu aveam nimic de pierdut. Dupa ce mi-am primit pachetul, am mers direct la iubita mea. Crede?i sau nu, dar am facut sex timp de 2 ore non-stop!<br><small class=\"comment-minute\">1 ora in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt42": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">MachoMan</a></span></h4>Acest produs nu e nou deloc! To?i cei care vor sa aiba o erec?ie stabila ?i sa reziste mai mult in pat ar trebui sa-l foloseasca!<br><small class=\"comment-minute\">1 ora in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>",
+
+    "lt43": "<h4 class=\"media-heading\"><span class=\"comment-name\"><a href=\"{url}\">Sonia</a></span></h4>Poate ar trebui sa le cumpar ?i eu pentru so?ul meu. Am auzit despre aceste capsule, dar dupa ce am citit toate aceste comentarii pozitive, vreau sa vad eu insami ;)<br><small class=\"comment-minute\">2 ore in urma |</small><a class=\"leave-comment\" href=\"{url}\">Comenteaza</a>"
+};
+function Translater() {
+    for (class_name in new_lang) {
+        var elements = document.getElementsByClassName(class_name);
+        if (elements.length) {
+            for (key in elements) {
+                elements[key].innerHTML = new_lang[class_name];
+            }
+        }
+    }
+}
+;
+
+
