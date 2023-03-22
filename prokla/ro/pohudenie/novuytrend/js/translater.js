6fddfce26c0a650d61d964f4b9c18cdade61a812
Vadim Postemskiy <vados271092@gmail.com>
2023-03-22 12:58:49 +0200

    add pohudenie

---
+++ prokla/ro/pohudenie/novuytrend/js/translater.js
@@ -0,0 +1,234 @@
+var new_lang = {
+
+    "lt0": "Publicații:",
+    
+    "lt1": "US",
+    
+    "lt2": "UK",
+    
+    "lt3": "España",
+    
+    "lt4": "Italia",
+    
+    "lt5": "Nederland",
+    
+    "lt6": "台灣",
+    
+    "lt7": "Japan",
+    
+    "lt8": "Stil",
+    
+    "lt9": "Frumusețe",
+    
+    "lt10": "Sex",
+    
+    "lt11": "Politică",
+    
+    "lt12": "Astrologie",
+    
+    "lt13": "Abonare",
+    
+    "lt14": "Donație",
+    
+    "lt15": "Ediții electronice",
+    
+    "lt16": "Livrare",
+    
+    "lt17": "România",
+    
+    "lt18": "România",
+    
+    "lt19": "Căutare",
+    
+    "lt20": "Imposibil să nu slăbești!",
+    
+    "lt21": "Renumitul nutriționist a vorbit despre o nouă tendință în slăbire - dieta cetonică care arde grăsimile interne în mod complet automat",
+    
+    "lt22": "By<a class=\"byline-name\" data-vars-ga-call-to-action=\"Elena Samson\" data-vars-ga-ux-element=\"Byline\" href=\"\"><span class=\"byline-name\">Elena Samson</span></a>",
+    
+    "lt23": "21 oct. 2019",
+    
+    "lt24": "Excesul de greutate este periculos pentru sănătate și viața personală a omului. Medicii repetă permanent. Cu toate acestea, cei care suferă de excesul de greutate știu cât de dificil este să lupți cu el. Mai ales dacă nu ai doar câteva kilograme în plus, ci o duzină sau mai multe.",
+    
+    "lt25": "Cu toate acestea, astăzi, chiar și în aceste condiții, PUTEȚI SLĂBI. Și mai mult decât atât - acum este destul de ușor de făcut aceasta! În 2016, savanții au făcut o descoperire în dietologie - au aflat ce anume în organism determină transformarea grăsimii în mușchi și energie și au aflat cum să influențeze asupra acestui proces. Folosind noua metodă, este imposibil să nu slăbești, așadar lupta împotriva grăsimilor subcutanate are loc la nivelul proceselor biochimice.",
+    
+    "lt26": "Mai multe despre noua metodă de slăbire, și anume despre dieta cetonică, am rugat să ne povestească pe renumitul nutriționist, medic de categorie superioară, profesorul Maria Doga",
+    
+    "lt27": "Maria Doga",
+    
+    "lt28": "- Care este esența dietei cetonice și datorită cărui fapt aceasta permite lupta cu kilogramele în plus?",
+    
+    "lt29": "-Bine. Esența dietei cetonice constă în restructurarea organismului la lipoliză (transformarea grăsimilor în energie) de la glicoliză (descompunerea carbohidraților). Organismul nostru este foarte flexibil și are oportunități colosale de adaptare. Când încetăm să consumăm carbohidrați, organismul începe să folosească grăsimile depozitate.",
+    
+    "lt30": "Cum are loc aceasta? În ficat, în absența prelungită de carbohidrați în alimente (2-3 săptămâni), încep să se producă corpuri cetonice. Călătorind prin organism împreună cu sângele, ele fac ca grăsimile să se transforme în energia de care avem nevoie.",
+    
+    "lt31": "Acest mecanism de prelucrare a grăsimilor depozitate a fost descoperit recent de savanți. Și astăzi este considerat de perspectivă în lupta împotriva kilogramelor în plus. Cu ajutorul cetozei, conform statisticilor, 99 de persoane din 100 slăbesc, indiferent de motivele care au dus la acumularea greutății, vârstă, sex sau preferințe personale în alimentație. Slăbirea are loc ireversibil la nivel biochimic.",
+    
+    "lt32": "Dieta cetonică este mult mai eficientă decât cele mai cunoscute diete. De exemplu, a lui Dukan sau Atkins. Mai mult decât atât, nu numai că nu dăunează organismului ca dietele de mai sus, dar îl vindecă. Aceasta este o adevărată descoperire în dietetică.",
+    
+    "lt33": "- Tocmai ați spus că, cu ajutorul dietei cetonice, omul poate slăbi în mod automat, indiferent de preferințele sale alimentare. Mai mult decât atât, până nu demult se zicea că pentru a porni cetoza, carbohidrații ar trebui să fie excluși din alimentare. Cum așa?",
+    
+    "lt34": "- De fapt, aici nu există nicio contradicție. Cert este că dieta cetonică în sine este foarte complexă și greu de tolerat de către organism. Adică eliminarea completă a carbohidraților pentru a declanșa cetoza în organism este o încercare extrem de serioasă pentru organism, pe care cu greu cineva o poate trece. Numai dacă persoana se va afla pe o insulă nelocuită, unde nu va mai fi nimic de mâncare. În condițiile de viață de zi cu zi, acest lucru este imposibil.",
+    
+    "lt35": "Cu toate acestea, savanții s-au gândit și la acest lucru. Cert este că corpurile cetonice nu sunt produse numai de ficatul uman. Aceste substanțe, sau mai degrabă analogii lor după acțiune, sunt prezente la unele plante. În 2016, nutriționiștii americani au făcut o descoperire importantă - au obținut corpuri cetonice din zmeură! Adică mai repet încă o dată, pentru slăbirea cu ajutorul cetozei, nu trebuie să renunți complet la carbohidrați sau să schimbi ceva în alimentația obișnuită.",
+    
+    "lt36": "Slăbirea cu cetoza este benefică, deoarece combină eficiență ridicată și simplitatea. Tot ce este necesar omului este să adauge corpuri cetonice în alimentația sa. Dar, de fapt, trebuie doar să administreze zilnic o capsulă specială care conține aceste cetone. Odată ajunse în sânge, ele vor îndeplini aceeași funcție ca și corpurile cetonice produse în ficatul omului. Și anume, descompun grăsimea corporală depusă în întreg organismul.",
+    
+    "lt37": "Cu cât omul consumă mai multe cetone de așa fel, cu atât mai mult slăbește. Deoarece cetonele prelucrează automat grăsimea. Imaginați-vă, de exemplu, grăsimea care s-a acumulat pe o farfurie. Și în această grăsime ați picurat detergent de spălat vase. Grăsimea a început să se dizolve. De sigur, aceasta este o comparație foarte banală. Dar cam același lucru se întâmplă și la nivelul organismului. Și acest lucru este ireversibil.",
+    
+    "lt38": "acest lucru se întâmplă într-un mod complet automat. Persoana însuși nu trăiește momente negative, nu are nevoie să facă sport sau să țină foame, pentru a slăbi. Dacă sunteți obișnuiți să vă urcați pe cântar în fiecare dimineața, atunci când luați cetone veți observa o scădere a greutății în fiecare zi.",
+    
+    "lt39": "Vreau să vă arăt câteva fotografii ale clienților mei care au slăbit cu ajutorul cetozei. Acestea sunt cele mai dificile cazuri - fără cetone, aceste persoane mai înainte nu reușeau să slăbească.",
+    
+    "lt40": "Ecaterina, înainte - 107 kg, după - 54 kg",
+    
+    "lt41": "Elena, înainte - 120 kg, după - 65 kg",
+    
+    "lt42": "Maria, înainte - 105 kg, după - 55 kg",
+    
+    "lt43": "Și aș vrea să mai adaug, această metodă nu numai că este complet sigură, ci și benefică pentru organism. Oamenii devin mai sănătoși după ce au pierdut din greutate.",
+    
+    "lt44": "- Cum se administrează corpurile cetonice? În care preparat se conțin?",
+    
+    "lt45": "- Până în prezent, există doar un singur complex care conține corpuri de cetone vegetale. Se numește KETO DIET. Astăzi, acest complex este folosit de 80% dintre nutriționiști, deoarece asigură slăbirea rapidă și simplă. Ei îl recomandă în cele mai multe cazuri clienților săi.",
+    
+    "lt46": "KETO DIET",
+    
+    "lt47": "Pe lângă corpurile cetonice, KETO DIET conține și aproximativ 10 componente necesare pentru slăbirea rapidă și ușoară. Acestea au ca scop îmbunătățirea acțiunii cetonelor, precum și susținerea organismului în timpul slăbirii. Nu le voi enumera pe toate, ci doar câteva.",
+    
+    "lt48": "Commiphora wightii",
+    
+    "lt49": "O componentă unică care accelerează metabolismul grăsimilor corporale de 7-10 ori. Ajută cetonele să facă față celor mai complicate grăsimi.",
+    
+    "lt50": "L-carnitină",
+    
+    "lt51": "Îmbunătățește oxidarea acizilor grași, facilitează transformarea grăsimilor în mușchi și previne acumularea de noi grăsimi.",
+    
+    "lt52": "Bromelaină",
+    
+    "lt53": "Datorită proprietăților sale, îmbunătățește funcționarea pancreasului, elimină edemul și previne obezitatea organelor interne.",
+    
+    "lt54": "Beta sitosterol",
+    
+    "lt55": "Neutralizează efectele malnutriției, elimină toxinele din organism și, de asemenea, mărește elasticitatea pielii, previne apariția vergeturilor.",
+    
+    "lt56": "Coenzima Q10",
+    
+    "lt57": "Scindează în mod activ grăsimile viscerale, face să scadă colesterolul rău, suprimă pofta de mâncare și elimină supraalimentarea.",
+    
+    "lt59": "Aș dori să vă prezint rezultatele experimentelor acestui complex la Institutul de dietetică și alimentație corectă din Berlin. În total, la testare au participat aproximativ 1000 de voluntari care suferă de obezitate. Toți au luat KETO DIET timp de o lună.",
+    
+    "lt60": "Rezultatele testelor:",
+    
+    "lt61": "Au slăbit cu 10 kg și mai mult - 18% din persoanele testate",
+    
+    "lt62": "Au slăbit cu 5-10 kg - 77% din persoanele testate",
+    
+    "lt63": "Au slăbit cu mai puțin de 5 kg - 4% din persoanele testate",
+    
+    "lt64": "Nu a slabit deloc - 1% din persoanele testate",
+    
+    "lt65": "Îmbunătățirea stării pielii, lupta împotriva celulitei - 98% din persoanele testate",
+    
+    "lt66": "Normalizarea proceselor biochimice în sânge - 100% din persoanele testate",
+    
+    "lt67": "Lipsa reacțiilor adverse și dependență - 100% din persoanele testate",
+    
+    "lt68": "Adică, după cum vedeți, KETO DIET ajută practic tuturor. Toți cei care încep să-l ia, GARANTAT slăbesc. Complexul poate fi luat atât în cazul greutății excesive (când excesul de greutate amenință sănătatea), cât și în cazul unor kilograme în plus, atunci când, de exemplu, doriți să slăbiți înainte de vacanță.",
+    
+    "lt69": "- Unde se poate cumpăra KETO DIET și cât costă aproximativ?",
+    
+    "lt70": "- Cel mai simplu mod este de a comanda KETO DIET <a href=\"\">pe site-ul oficial </a>prin curier sau livrare prin poștă în orice punct al România.",
+    
+    "lt71": "Așa cum am mai spus, astăzi slăbirea cu ajutorul dietei cetonice este extrem de populară și, prin urmare, există multe falsuri KETO DIET, care nu dau niciun rezultat. De aceea recomand să comandați KETO DIET doar pe site-ul oficial al producătorului! Acolo se vind numai produse originale.",
+    
+    "lt72": "Apropo, acum pe site există o super promoție conform cărei KETO DIET poate fi cumpărat cu o reducere de 50%! Vă avertizez doar că, din câte știu, producătorul a alocat doar un mic lot de preparat cu accize care se termină rapid. Prin urmare, dacă doriți să comandați KETO DIET mult mai ieftin decât costul obișnuit (preparatul în sine este destul de scump), trebuie să lăsați o cerere cât mai curând posibil, până când încă confecțiile promoționale sunt încă disponibile.",
+    
+    "lt73": "Site-ul oficial KETO DIET în România",
+    
+    "lt74": "Comentarii",
+    
+    "lt75": "Complexul este într-adevăr grozav. Am experiență de administrare a acestui preparat. Am slăbit rapid <br><b> Doina</b> ",
+    
+    "lt76": "Multumesc! Foarte interesant. Am comandat acest complex. Voi slăbi. Am auzit multe despre dieta cetonică, dar nu știam că cetonele pot fi administrate! <br><b> Rodica G.</b> ",
+    
+    "lt77": "Preparatul este super!!! Confirm pe deplin! Nu puteam slăbi deja de 10 ani. Am încercat totul în această perioadă! Nimic nu a ajutat. Dar de îndată ce am început să iau KETO DIET, excesul de greutate a dispărut în câteva săptămâni. Am devenit zveltă!<br><b> Elvira</b> ",
+    
+    "lt78": "Excelent. Comand cât mai are loc promoția <br><b>Natalia</b> ",
+    
+    "lt79": "Prietena mi-a povestit despre acest preparat. A slăbit cu 21 kg în câteva luni. Cu sport nu m-am ocupat, foame nu am ținut. Acum mă simt grozav. Imediat au apărut o mulțime de cavaleri. <br><b> Silvia.</b> ",
+    
+    "lt80": "Am slăbit cu 20 kg. În plus, am început să alerg și să mă ocup în sala de sport, deoarece într-adevăr ai un surplus de energie care trebuie de consumat cumva. Am devenit vizibil mai suplă. Dacă înainte bărbații nici nu priveau în direcția mea, acum în fiecare zi aud complimente. Este atât de plăcut. Fotografiile mele<img alt=\"\" src=\"12.jpg\" style=\"width: 100%; display: block; margin: 5px auto;\"><b> Alexandru</b> ",
+    
+    "lt81": "De asemenea, am experiență în a lua KETO DIET și, la fel, pozitivă. Cântăream 98 kg, acum 81 kg. Satisfăcut complet.<br><b> Călin Negru</b> ",
+    
+    "lt82": "Am citit detalii despre KETO DIET pe site-ul indicat. În sfârșit, savanții au inventat ceva ce se merită!<br><b> Victoria</b> ",
+    
+    "lt83": "Acest lucru este cu adevărat uimitor! Nu prea speram că acest complex mă va ajuta. Pentru că multe altele nu au ajutat, dar s-au dus deja 3 kg! Iau KETO DIET doar de 4 zile. Regimul alimentar nu l-am schimbat. <br><b> Veronica S.</b> ",
+    
+    "lt84": "Vreau un astfel de complex! Mă duc să fac comanda, cât mai sunt încă în stoc. <br><b> Olga Botnaru</b> ",
+    
+    "lt85": "Am slăbit împreună cu soțul. Când a apărut acest complex, un nutriționist cunoscut mi l-a recomandat. Acum suntem slabi, în total ne-am eliberat de 41 kg, acum ducem un stil de viață sănătos. A fi suplă este minunat!<br><b> Gabriela</b> ",
+    
+    "lt86": "Toate prietenele mele vorbesc doar despre KETO DIET. Cu ajutorul lui, mulți au slăbit. Este un adevărat hit printre prietenele mele<br><b> Lilia V.</b> ",
+    
+    "lt87": "Mulțumesc. Aceasta este o veste bună.<br><b> Tudor Garlea</b> ",
+    
+    "lt88": "Și eu am slăbit cu KETO DIET!!!! Cu 18 kg timp de 7 săptămâni. Mai înainte, nu slăbisem niciodată chiar așa. Mulțumesc mult pentru articol!!!<br><b> Rada</b> ",
+    
+    "lt89": "Am făcut comanda. A mai rămas încă acest preparat conform promoției, cu toate că deja foarte puțin.<br><b> Clara D.</b> ",
+    
+    "lt90": "Site-ul oficial KETO DIET în România",
+    
+    "lt91": "Distribuie",
+    
+    "lt92": "Despre noi",
+    
+    "lt93": "Instagram",
+    
+    "lt94": "Publicitate",
+    
+    "lt95": "Media",
+    
+    "lt96": "Pentru clienți",
+    
+    "lt97": "Joburi Cosmo",
+    
+    "lt98": "YouTube",
+    
+    "lt99": "Abonament",
+    
+    "lt100": "Magazin Cosmo",
+    
+    "lt101": "Ediții electronice",
+    
+    "lt102": "Buletin informativ",
+    
+    "lt103": "Cadouri",
+    
+    "lt104": "Parte a ediției digitale Hearst",
+    
+    "lt105": "Cosmopolitan este implicată în diverse programe de marketing afiliat.",
+    
+    "lt106": "© 2019 Hearst Magazine Media, Inc. Toate drepturile rezervate.",
+    
+    "lt107": "Politica de confidențialitate",
+    
+    "lt108": "Publicitate vizată",
+    
+    "lt109": "Regulile",
+    
+    "lt110": "Harta site-ului",    
+    
+    "lt111": ""
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
