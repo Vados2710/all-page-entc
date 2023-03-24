a076493af0fc42434988ad143087cf43cf734ba9
Lenovo <ir.yastrebova@gmail.com>
2023-03-24 10:46:26 +0200

    gribok

---
+++ prokla/ro/gribok/kak-izbavitsya/js/translater.js
@@ -0,0 +1,178 @@
+var new_lang = {
+  "lt1": "acasă",
+
+  "lt2": "rating-ul înregistrărilor",
+  
+  "lt3": "Login",
+  
+  "lt4": "CREAZĂ UN BLOG",
+  
+  "lt5": "POLITICA COMUNITĂȚII",
+  
+  "lt6": "LUME",
+  
+  "lt7": "CINEMA",
+  
+  "lt8": "ȘTIINȚĂ ȘI TEHNOLOGIE",
+  
+  "lt9": "DIVERTISMENT",
+  
+  "lt10": "FAMILIE ȘI COPII",
+  
+  "lt11": "NUTRIȚIE",
+  
+  "lt12": "SĂNĂTATE ȘI FRUMUSEȚE",
+  
+  "lt13": "STIL DE VIAȚĂ",
+  
+  "lt14": "20 de ani de LJ",
+  
+  "lt16": "pe pagina principală",
+  
+  "lt17": "doctor_blog",
+  
+  "lt18": "Timp de citire 5 minute",
+  
+  "lt19": "Această postare ocupă locul 156 în rating",
+  
+  "lt20": "LJ recomandă",
+  
+  "lt24": "Categorie:",
+  
+  "lt25": "Sănătate",
+  
+  "lt26": "Anulează",
+  
+  "lt27": "Ciuperca unghiilor: ce pericol reprezintă această boală și cum să scapi de ea?",
+  
+  "lt28": "Bună ziua! Mă numesc Grigore Platon, eu sunt un dermatolog practicant cu o experiență de 25 de ani. ",
+  
+  "lt29": "Mă întâlnesc zilnic cu pacienții care suferă de ciuperca unghiilor. Este uimitor, dar mulți nici măcar nu știu despre pericolul unei astfel de boli – acest fapt m-a împins să scriu acest articol.",
+  
+  "lt30": "Potrivit statisticilor, fiecare al cincilea locuitor al pământului suferă de ciupercă unghiilor, sau onicomicoză, termenul oficial. Dar, în ciuda prevalenței acestei boli, aceasta nu este deloc inofensivă. Ciuperca unghiilor nu poate fi ignorată și lăsată netratată.",
+  
+  "lt31": "Ce se întâmplă dacă ciuperca nu este tratată?",
+  
+  "lt32": "Ciuperca este un inamic periculos al organismului. Faptul este că, în procesul vieții sale, ciupercile eliberează toxine care reduc imunitatea. În consecință, se dezvoltă boli ale organelor interne și reacții alergice severe.",
+  
+  "lt33": "La persoanele cu vârsta peste 40 de ani, o infecție fungică este una dintre principalele cauze ale răcelilor frecvente, astmului bronșic, tulburări ale organelor interne. La această vârstă, infecția pe unghie poate duce la dermatită pe tot piciorul inferior. Prin urmare, nu trebuie să amânați tratamentul ciupercii. Boala avansată duce la o formă distrofică totală a onicomicozei, care provoacă complicații care pun viața în pericol.",
+  
+  "lt34": "Ce complicații cauzează ciuperca?",
+  
+  "lt35": "În procesul de viață, ciupercile eliberează substanțe asemănătoare antibioticelor: xantomegnină, penicilină, viomeleină și altele. Intrând în organism, aceste toxine provoacă boli somatice severe. Pentru a ilustra acest lucru, vedeţi fotografii:",
+  
+  "lt36": "Dermatită toxică-alergică",
+  
+  "lt37": "Sindromul Lyell (necroliză epidermică toxică)",
+  
+  "lt38": "Trebuie să începeți să luptați cu ciuperca în stadiile incipiente ale infecției, deoarece cu sânge ciuperca se răspândește la organele interne. Pe fondul imunității slăbite, se dezvoltă complicații infecțioase severe: leziuni toxico-alergice ale inimii, ficatului, organelor abdominale, rinichilor.",
+  
+  "lt39": "Ei bine, desigur, dacă nu începeți să tratați ciuperca în stadiile incipiente, în câțiva ani ea distruge complet structura plăcii unghiilor și duce la pierderea acesteia.",
+  
+  "lt40": "Efectele ciupercii de pe placa unghiului avansate",
+  
+  "lt41": "În ciuda tuturor celor de mai sus, există o opinie comună că apariția ciupercilor pe unghii și pe pielea picioarelor este doar o problemă estetică care, în majoritatea cazurilor, este asociată cu modificări legate de vârstă în organism. Aceasta este o concepție greșită.",
+  
+  "lt42": "Cum se trateze ciuperca?",
+  
+  "lt43": "Cu câțiva ani în urmă, tratarea ciupercii unghiei a avut un caracter complex și a durat cel puțin un an. A fost necesar să se trateze nu numai prin utilizarea externă, ci și prin administrarea antibioticelor în interior. Vă dați seama care este sarcina asupra ficatului și a organelor interne? Să luați cele mai puternice antibiotice pentru o perioadă atât de lungă!",
+  
+  "lt44": "Și numai în 2018, oamenii de știință de la Institutul Național de sănătate au dezvoltat un medicament care permite eliminarea fără durere a ciupercilor unghiilor în cel mai scurt timp. Remediul a fost numit XX.",
+  
+  "lt45": "Acest medicament ucide sporii fungici și oprește răspândirea lor prin localizarea zonelor inflamate. Ameliorează mâncărimea, durerea și iritarea. Repară celulele afectate de infecție, elimină mirosul rău cauzat de bacterii. Stimulează regenerarea unghiilor, astfel încât zonele afectate ale unghiilor cresc mai repede.",
+  
+  "lt46": "Astfel, timpul de tratament al ciupercii unghiilor este redus de 10-12 ori!",
+  
+  "lt47": "Un alt moment plăcut: XX este un remediu complet natural, nici unul dintre componentele sale nu produce efecte secundare.",
+  
+  "lt48": "XX este vândut în farmacii, dar va fi destul de dificil să-l găsiți: acesta este un medicament nou și este destul de rar. Prin urmare, compania-producător a creat un site unde puteți comanda XX la un cost de producție, fără marje de revânzători. Împărtășesc link-ul:",
+  
+  "lt49": "Cumpără XX cu reducere pe site-ul producătorului",
+  
+  "lt53": "doctor_blog",
+  
+  "lt54": "Adaugă la prieteni",
+  
+  "lt55": "MAI MULTE LUCRURI INTERESANTE PENTRU DVS.",
+  
+  "lt56": "a_nalgin <span class=\"itemTimereading\">Timp de citit 5 minute </span> ",
+  
+  "lt57": "Despre păcatul tinereții că o fată este permisă ...",
+  
+  "lt58": "bookmatejournal <span class=\" itemTimereading\">Timp de citit 9 minute</span> ",
+  
+  "lt59": "Angelina Jolie a recunoscut pentru prima dată ...",
+  
+  "lt60": "fomaru <span class=\"itemTimereading\">Timp de citit 10 minute</span> ",
+  
+  "lt61": "Îngrijirea vârstnicilor: caracteristici și reguli",
+  
+  "lt62": "gelio <span class=\"itemTimereading\">Timp de citit 19 minute</span> ",
+  
+  "lt63": "Privelişte de sus: cel mai mare oraș ...",
+  
+  "lt64": "Comentarii",
+  
+  "lt65": "Eugen Chiper",
+  
+  "lt67": "Am cumpărat nu din cauza ciupercii. Am suferit mult timp de un miros neplăcut de la picioare și disconfort. Am început să folosesc XX, după câteva săptămâni totul a dispărut",
+  
+  "lt68": "Victoria Ioniță",
+  
+  "lt70": "Mulțumesc pentru recomandare. M-am obosit să lupt deja cu ciuperca…",
+  
+  "lt71": "Elena Catruc",
+  
+  "lt73": "E un remediu excelent. L-am folosit cu soțul meu, amândoi aveam o ciupercă (el a fost primul infectat și apoi m-a infectat). Am folosit împreună pentru a se vindeca imediat și pentru a nu se infecta din nou. Vreau să spun, XX funcționează perfect. Ciuperca a dispărut după 3-4 săptămâni",
+  
+  "lt74": "Dumitru Galațchi",
+  
+  "lt76": "Mulțumesc pentru sfat!",
+  
+  "lt77": "Svetlana Parfeni",
+  
+  "lt79": "Vara vreau cu adevărat să port pantofi frumoase deschise, dar nu-mi pot permite din cauza ciupercii...  m-am luptat cu ea atât de mult timp - m-am obosit deja, nimic nu ajută! Sper foarte mult pentru XX",
+  
+  "lt80": "Gloria Sârbu",
+  
+  "lt82": "mi-am comandat un XX, mi-au în a doua zi prin curier. Am început să-l folosesc și toate problemele cu picioarele au trecut! Mi-e frică să-mi amintesc cum am trăit cu asta.",
+  
+  "lt83": "Ion Golban",
+  
+  "lt85": "Vă mulțumesc pentru acest articol, e foarte informativ! Am comandat XX",
+  
+  "lt86": "Lilia Zaporojan",
+  
+  "lt88": "Oh! Și eu am fost tratată cu acest remediu!! XX este cel mai bun!",
+  
+  "lt89": "Lucian Cârstea",
+  
+  "lt91": "Mulțumesc!",
+  
+  "lt92": "Elisabeta Casian",
+  
+  "lt94": "Vă mulțumesc pentru link-ul către site-ul producătorului",
+  
+  "lt95": "Vrei să scrii un mesaj?",
+  
+  "lt96": "Trimite",
+  
+  "lt97": "Despre",
+  
+  "lt98": "Acordul utilizatorului",
+  
+  "lt99": "Ajutor",
+  
+  "lt100": "Buton \"Share\""
+  
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
---
+++ prokla/ro/gribok/sovety/js/translater.js
@@ -0,0 +1,156 @@
+var new_lang = {
+  "lt0": "ABONEAZĂ-TE",
+
+  "lt2": "Sfatul dermatologului: cum să tratați ciuperca unghiilor",
+  
+  "lt3": "Data publicării: <span class=\"post-date\">30.06.2020</span> ",
+  
+  "lt4": "Ciupercă",
+  
+  "lt5": "Tratament și prevenire",
+  
+  "lt7": "Ciuperca unghiilor este o problemă care, potrivit statisticilor, deranjează fiecare a cincea persoană. Trist, dar puteți infecta în piscină, pe plajă și în dușuri publice, saune, vestiare – într-un cuvânt, peste tot.",
+  
+  "lt8": "Am vorbit cu Igor Țopescu, profesor la Departamentul de Dermatologie al Institutului Național de Sănătate, despre cum să abordăm ciuperca unghiilor.",
+  
+  "lt9": "Dumitru Rusnac",
+  
+  "lt10": "- Igor, ce este această boală - ciuperca unghiilor? Cum se dezvoltă?",
+  
+  "lt11": "- Ciuperca unghiilor este distrugerea plăcii unghiilor sub acțiunea diferitelor tipuri de ciuperci patogene. Adesea, oamenii ignoră debutul bolii, fără a acorda importanță primelor simptome - descuamarea și fisuri pe pielea degetelor și între ele. În continuare, agenții patogeni ai bolii sunt introduși în unghie, de ce apar striații și galbeneală. Apoi, unghia se rupe și se desprinde.",
+  
+  "lt12": "- Care este pericolul infecțiilor fungice?",
+  
+  "lt13": "- În primul rând, unghiile afectate de ciuperci sunt un focar deschis de infecție care poate afecta alte unghii sănătoase. De asemenea, este foarte probabil să vă infectați membrile de familie și pe cei din jur. Leziunile fungice ale unghiilor complică cursul diabetului zaharat și varicelor, este poarta de intrare a unei infecții bacteriene. Onicomicoza poate duce la pierderea completă a plăcii de unghii fără capacitatea de regenerare a acesteia. Adică, vă pierdeți unghiile și ele nu mai cresc niciodată. Nu numai că nu este frumos din punct de vedere estetic, dar provoacă, de asemenea, o mulțime de inconveniente în viața de zi cu zi, deoarece unghiile protejează plăcuțele sensibile ale degetelor.",
+  
+  "lt14": "Exemple de ciuperci de unghii neglijate:",
+  
+  "lt15": "- Cum să faceți față ciupercii unghiilor?",
+  
+  "lt16": "- Anterior, tratarea onicomicozei a fost complexă și a durat cel puțin un an.</b> A fost necesar să se trateze nu numai prin utilizarea externă, ci și prin administrarea antibioticelor în interior. Vă dați seama care este sarcina asupra ficatului și a organelor interne? Să luați cele mai puternice antibiotice pentru o perioadă atât de lungă!",
+  
+  "lt17": "Prin urmare, oamenii de știință din departamentul nostru au avut sarcina de a dezvolta un medicament sigur de acțiune locală care să se lupte cu ciuperca în cel mai scurt timp.",
+  
+  "lt18": "- Ați reușit să dezvoltați acest medicament?",
+  
+  "lt19": "- Da, remediul a fost numit XX. Elimină toate tipurile cunoscute și periculoase de infecții fungice; ameliorează mâncărimea, durerea și iritarea. Localizează zonele de inflamație (datorită cărora ciuperca nu se răspândește). Repară celulele afectate de infecție, elimină mirosul rău cauzat de bacterii. Stimulează regenerarea unghiilor, astfel încât zonele afectate ale unghiilor cresc mai repede.",
+  
+  "lt20": "Astfel, timpul de tratament al ciupercii unghiilor este redus de 10-12 ori!",
+  
+  "lt21": "Iată sunt aceste date pe care le-am obținut prin efectuarea studiilor clinice ale acestui remediu:",
+  
+  "lt22": "Eliminarea ciupercii picioarelor și unghiilor -100% dintre subiecți",
+  
+  "lt23": "Eliminarea mâncărimii, descuamării și crăpăturilor ale pielii – 95% dintre subiecți",
+  
+  "lt24": "Eliminarea mirosului neplăcut și a transpirației - 90% dintre subiecți",
+  
+  "lt25": "Boala a fost învinsă în mai puțin de o lună – 98% dintre subiecți",
+  
+  "lt26": "Un alt moment plăcut: XX este un remediu complet natural, nici unul dintre componentele sale nu produce efecte secundare.",
+  
+  "lt27": "- Unde se poate cumpăra XX?",
+  
+  "lt28": "- XX este vândut în farmacii, dar va fi destul de dificil să-l găsiți: acesta este un medicament nou și este destul de rar. Prin urmare, compania-producător a creat un site unde puteți comanda XX la un cost de producție, fără marje de revânzători.",
+  
+  "lt29": "- Igor, vă mulțumim pentru un interviu semnificativ. Cred că ați ajutat mulți dintre cititorii noștri.",
+  
+  "lt30": "<b>De la redacție:</b> link-ul către site-ul unde puteți cumpăra XX la prețul de producție, este atașat de mai jos.",
+  
+  "lt31": "Să cumpărați XX pe site-ul producătorului cu o reducere",
+  
+  "lt32": "+ 5 resurse",
+  
+  "lt33": "RECENZIE:",
+  
+  "lt34": "Comentarii:",
+  
+  "lt35": "Maria Furculița",
+  
+  "lt37": "Vă mulțumesc pentru recomandare. M-am obosit să lupt deja cu ciuperca…",
+  
+  "lt38": "Elisabeta Danici",
+  
+  "lt40": "Soțul meu a suferit de o ciupercă timp de 3 ani, este un coșmar, unghiile au devenit largi, îngroșate, nici măcar nu pot descrie. La început dădeam cu picături, apoi alte picături - nu-mi amintesc numele lor. În cele din urmă, soțul a înnebunit și a renunțat, deoarece nimic nu a ajutat. A încercat cineva XX? Chiar funcționează?",
+  
+  "lt41": "Grigore Gherasim",
+  
+  "lt43": "Luați cu siguranță. Am mai avut un miros. A trecut și el. Principalul lucru, asigurați-vă ca soțul să ungă în fiecare seară.",
+  
+  "lt44": "Liliana Topal",
+  
+  "lt46": "Oh! Și eu am fost tratată cu acest remediu!! XX este cel mai bun!",
+  
+  "lt47": "Radu Nenicul",
+  
+  "lt49": "Elisabeta, de asemenea, m-a ajutat foarte mult acest remediu",
+  
+  "lt50": "Gheorghe Spătaru",
+  
+  "lt52": "Vă mulțumesc pentru articol, e foarte informativ! Am comandat XX",
+  
+  "lt53": "Veronica Bordeniuc",
+  
+  "lt55": "De asemenea, am comandat! Sunt cei care au primit coletul? Cât durează livrarea?",
+  
+  "lt56": "Vladislav Slătineanu",
+  
+  "lt58": "Veronica, mi-au livrat în trei zile chiar la ușă",
+  
+  "lt59": "Margareta Ababii",
+  
+  "lt61": "Aș dori, de asemenea, să port sandale deschise...nu pot. Mi-e rușine să-mi arăt picioarele. În timpul verii, vreau atât de mult să port pantofi deschise frumoși. Mă uit la femei - invidiez! Nu știu dacă mă va ajuta...am fisuri pe călcâi, unghiile sunt galbene:((((",
+  
+  "lt62": "Andrei Chelariu",
+  
+  "lt64": "Mulțumesc!",
+  
+  "lt65": "Data publicării: <span class=\"post-date\">30.06.2020</span> ",
+  
+  "lt66": "Poate fi interesant",
+  
+  "lt67": "Cum să scăpați de acnee subcutanată: 6 moduri",
+  
+  "lt68": "Cum să preveniți apariția acneei",
+  
+  "lt69": "Cele mai bune vitamine și minerale împotriva acneei",
+  
+  "lt70": "Uleiuri esențiale pentru tratarea acneei",
+  
+  "lt71": "Mierea și scorțișoara pot trata acneea?",
+  
+  "lt72": "Despre noi",
+  
+  "lt73": "Articole <br>despre sănătate",
+  
+  "lt74": "Știri <br>despre sănătate",
+  
+  "lt75": "Contactele <br>noastre",
+  
+  "lt76": "Publicitate",
+  
+  "lt77": "Politica <br>de publicitate",
+  
+  "lt78": "Abonează-te<br> la actualizări",
+  
+  "lt79": "Carieră",
+  
+  "lt80": "Găsiți<br>un medic online",
+  
+  "lt81": "Ajutor",
+  
+  "lt82": "Politica <br>de confidențialitate",
+  
+  "lt83": "© 2005- <span class=\"footer-date\">2020</span> Healthline Media a Red Ventures Company. Toate drepturile rezervate. Serviciile, conținutul și produsele site-ului nostru au doar scopuri informative. Healthline Media nu oferă consultații medicale, diagnostic sau tratament. <span class=\"css-p0jwkp\">Aflați informații suplimentare</span> ."
+  
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
