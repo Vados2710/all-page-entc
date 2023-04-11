b25087c51737baf2d2ece197e35e13021734189c
Lenovo <ir.yastrebova@gmail.com>
2023-04-11 16:27:19 +0300

    lands ro sluh

---
+++ lands/ro/sluh/land1/js/translater.js
@@ -0,0 +1,270 @@
+var new_lang = {
+
+
+
+
+
+"lt00": "Cannabis Oil – Vă readuce posibilitatea de a auzi",
+
+"lt0": "Cannabis Oil",
+
+"lt1": "Scăderea auzului",
+
+"lt2": "Cauze",
+
+"lt3": "Compoziție",
+
+"lt4": "Părere",
+
+"lt5": "Păreri",
+
+"lt6": "Comandați",
+
+"lt7": "Vă readuce<span class=\"order__subtitle-right\">posibilitatea</span> de a auzi",
+
+"lt8": "Cannabis<span class=\"order__title-thing\">Oil</span> ",
+
+"lt9": "Rapid",
+
+"lt10": "Eficient",
+
+"lt11": "Fără operații",
+
+"lt12": "Preț cu reducere:",
+
+"lt15": "Până la sfârșitul promoției au mai rămas:",
+
+"lt16": "Zile",
+
+"lt17": "Ore",
+
+"lt18": "Minute",
+
+"lt19": "Secunde",
+
+"lt20": "Țara:",
+
+"lt22": "Numele dumneavoastră: <input class=\"form__input\">",
+
+"lt23": "Numărul dumneavoastră de telefon: <input class=\"form__input\">",
+
+"lt25": "Mergeți cu mouse-ul deasupra nivelurilor de zgomot",
+
+"lt26": "Uneori este atât de liniște în jur încât trebuie să ascultați",
+
+"lt27": "și uneori este atât de tare încât sună în urechi",
+
+"lt28": "5 dB",
+
+"lt29": "căderea fulgilor de zăpadă",
+
+"lt30": "10 dB",
+
+"lt31": "respirație",
+
+"lt32": "20 dB",
+
+"lt33": "șoapte",
+
+"lt34": "40 dB",
+
+"lt35": "conversație",
+
+"lt36": "60 dB",
+
+"lt37": "muzică la boxă",
+
+"lt38": "80 dB",
+
+"lt39": "plânset de copil",
+
+"lt40": "100 dB",
+
+"lt41": "concert",
+
+"lt42": "120 dB",
+
+"lt43": "bubuitură de tunet",
+
+"lt44": "130 dB",
+
+"lt45": "limita volumului urechii",
+
+"lt46": "220 dB",
+
+"lt47": "explozie atomică",
+
+"lt48": "315 dB",
+
+"lt49": "Căderea meteoritului Tunguska 30 iunie 1908 — cel mai puternic sunet înregistrat vreodată în istoria omenirii.",
+
+"lt50": "Intensitatea unui sunet în zona de percepție auditivă variază de la 0 până la 140 dB. Astfel, volumul unei șoapte este de — 20 dB, iar a unei conversații de — 40—60 dB. Sunetul la 120—130 dB provoacă supraîncărcare cu posibile traume auditive.",
+
+"lt51": "Problemele încep cu o șoaptă",
+
+"lt52": "Sunetul este oscilația particulelor în spațiu, care sunt captate de organele auzului. Dacă unul sau mai multe organe auditive sunt deteriorate, percepția sunetelor este distorsionată, înăbușită sau nu mai auzim cu totul.",
+
+"lt53": "Dacă nu puteți desluși sunetul unei șoapte sau nu îl auziți deloc, atunci auzul dumneavoastră este deja redus. Pentru a evita pierderea parțială sau completă a auzului, tratamentul trebuie început de la primul semn de scădere a auzului.",
+
+"lt54": "Cauzele scăderii auzului",
+
+"lt55": "Capacitatea de a asculta lumea înconjurătoare este un dar de la natură, de care vă puteți bucura toată viața, dar în același timp îl puteți pierde într-o clipă.",
+
+"lt56": "Scăderea auzului se împarte în două tipuri — conductiv și neurosenzorial.",
+
+"lt57": "Pierderea auzului neurosenzorial duce la probleme de auz la anumite frecvențe, afectând audibilitatea anumitor voci sau, de exemplu, a consoanelor. Pierderea auzului conductiv are ca rezultat, de obicei, un sunet înfundat.",
+
+"lt58": "Pierderea auzului senzorineural:",
+
+"lt59": "administrarea de medicamente, toxice pentru organele auditive (anumite antibiotice, antiinflamatoare etc.);",
+
+"lt60": "toxine industriale (benzen, anilină);",
+
+"lt61": "infecții (gripă, encefalită transmisă de căpușe, meningită, rujeolă, oreion, scarlatină, difterie, sifilis);",
+
+"lt62": "perturbarea metabolismului și dereglări vasculare (diabet zaharat, hipotiroidism, hipertensiune arterială, accident vascular cerebral);",
+
+"lt63": "tumori (neurinom acustic);",
+
+"lt64": "traume și vătămări (mecanice, expunerea la sunete puternice sau fluctuații de tensiune);",
+
+"lt65": "modificări naturale de vârstă.",
+
+"lt66": "Pierderea auzului conductiv:",
+
+"lt67": "dopuri de ceară;",
+
+"lt68": "defecte ale membranei timpanice rezultate din traumatisme sau otite;",
+
+"lt69": "traumatisme mecanice ale urechii, introducerea și extragerea unui corp străin;",
+
+"lt70": "hemoragie la nivelul urechii medii;",
+
+"lt71": "ruperea conexiunilor dintre osiculele auditive (traumatisme, inflamații);",
+
+"lt72": "tumori false (colesteatom);",
+
+"lt73": "tumori adevărate (cancer al canalului auditiv).",
+
+"lt74": "Readuceți-vă",
+
+"lt75": "capacitatea de a vă bucura de sunete",
+
+"lt76": "Pentru a restabili auzul, în primul rând, trebuie să eliminați cauzele care au dus la scăderea acestuia - scăpați de dopul care s-a format, tratați otita, nu mai ascultați muzică tare etc.",
+
+"lt77": "Dacă nu aveți o cauză clară sau dacă pierderea auzului este legată de vârstă, începeți să luați Cannabis Oil.",
+
+"lt78": "Cannabis Oil este un ulei de canabis concentrat în capsule, adaptat pentru administrarea în scopuri medicale. Fitocanabinoizii din compoziția Cannabis Oil sunt saturați cu un grup de vitamine și aminoacizi care acționează activ asupra regenerării accelerate a canalului auditiv.",
+
+"lt79": "Administrarea Cannabis Oil este sigură pentru organism.",
+
+"lt80": "Beneficiul canabisului este în fiecare capsulă",
+
+"lt81": "Substanțele speciale din compoziția uleiului de canabis acționează ca un element de reconstrucție a celulelor corpului uman.",
+
+"lt82": "Capsulele Cannabis Oil activează procesele de regenerare și restaurare a celulelor organelor auditive în caz de leziuni mecanice, traumatice, neurosenzoriale și infecțioase ale membranei timpanice, nervului auditiv, celulelor piloase.",
+
+"lt83": "Uleiurile active de canabis refac circulația sângelui și transmiterea impulsurilor sonore către creier, îmbunătățesc percepția sunetului. La o aplicare în decursul unui tratament – are loc restabilirea completă a auzului.",
+
+"lt84": "VĂ RUGĂM, ACORDAȚI ATENȚIE! Cantitatea de produse Cannabis Oil, necesară pentru rezultatul dorit, este determinată în mod individual și în cazuri speciale poate fi crescută.",
+
+"lt85": "Părerea expertului",
+
+"lt86": "Andrei Cazacu",
+
+"lt87": "Doctor în științe medicale, docent, vechime în muncă de 24 de ani.",
+
+"lt88": "Doctor în științe medicale, expert în domeniul diagnosticului, tratamentului și reabilitării tulburărilor de auz. Andrei Cazacu participă la simpozioane profesionale în calitate de lector. Portofoliul de lucrări științifice al specialistului cuprinde peste 50 de articole tipărite.",
+
+"lt89": "Problemele de auz pot apărea dintr-o varietate de motive, de la modificări legate de vârstă și gene, până la leziuni la cap și urechi, boli infecțioase și cronice. Prin urmare, oricine, la orice vârstă, poate experimenta pierderea auzului.",
+
+"lt90": "Persoana vine deja la consultație cu probleme serioase. Este rar ca cineva să abordeze problema la timp, pentru care ulterior trebuie să plătească cu măsuri extreme: operații, cure de medicamente puternice (în cazul infecțiilor virale) și așa mai departe. Majorității pacienților le este prescris aparatul auditiv – se întâmplă rar să fie restabilit un auz bun.",
+
+"lt91": "Dar acum totul este altfel. Datorită noilor remedii, refacerea auzului devine din ce în ce mai ușoară. Un lider printre acestea este – Cannabis Oil. În clinica noastră, tuturor pacienților li se prescrie ca adjuvant în reabilitarea auzului după intervenții chirurgicale, tratament complet și prevenire. În practică 8 din 10 pacienții și-au restabilit complet auzul doar datorită Cannabis Oil.",
+
+"lt92": "Ana Coman",
+
+"lt93": "35 de ani",
+
+"lt94": "Din copilărie am probleme cu urechile. Numai puțin dacă mă expun la frig, încep să mă doară urechile și-mi crește temperatura. Înainte trebuia mereu să-mi pun picături în urechi, ca să reduc din durere, de aceea m-am obișnuit ca tot timpul să aud sunete îndepărtate. Acum o jumătate de an am observat că chiar și cu picăturile acestea tot am probleme cu urechile. M-am dus la un specialist pe care-l cunoșteam, el mi-a recomandat să urmez un tratament cu Cannabis Oil. Așa și am făcut. Auzul a revenit 100%, acum mi se pare că aud chiar cum vecinii de la 2 etaje superioare respiră.",
+
+"lt95": "Maria Velișcu",
+
+"lt96": "44 de ani",
+
+"lt97": "Pe mine remediul Cannabis Oil m-a ajutat cu problemele de auz și desigur, nu e vreun remediu minune, dar își face efectul și-și face efectul bine. Mi s-a potrivit acest remediu, nu a provocat alergii, are rezultate. Per total, sunt mulțumită, pot recomanda.",
+
+"lt98": "Cristian Geambașu",
+
+"lt99": "57 de ani",
+
+"lt100": "Am cumpărat Cannabis Oil pentru tatăl meu, la bătrânețe auzul i s-a înrăutățit puternic. Cumva l-am forțat să urmeze acest tratament, mult timp s-a împotrivit. Dar acum măcar aude bine! Nu mai trebuie să-mi forțez corzile vocale când vorbesc cu el, iar când pornesc televizorul nu mai trebuie să dau volumul la maxim. Cu Cannabis Oil nu mai este nevoie să cumpărați un aparat auditiv.",
+
+"lt101": "Anghel Diaconu",
+
+"lt102": "53 de ani",
+
+"lt103": "Am cumpărat Cannabis Oil cu speranța disperată de a restabili auzul după o vătămare gravă a capului. Cu urechea stângă aproape că nu auzeam nimic, cu dreapta așa și așa. Chiar mă gândeam să-mi cumpăr un aparat auditiv, dar am decis să fac altceva – și anume să urmez un tratament cu Cannabis Oil, care mi l-a recomandat un doctor bun. Așa că tratamentul cu acest remediu m-a ajutat să-mi readuc auzul! Nu cu 100%, dar aud cu urechea stângă și dreaptă aproape la fel și foarte bine, dacă e să compar cu ce am avut înainte de acest tratament.",
+
+"lt104": "Ana Coman",
+
+"lt105": "35 de ani",
+
+"lt106": "Din copilărie am probleme cu urechile. Numai puțin dacă mă expun la frig, încep să mă doară urechile și-mi crește temperatura. Înainte trebuia mereu să-mi pun picături în urechi, ca să reduc din durere, de aceea m-am obișnuit ca tot timpul să aud sunete îndepărtate. Acum o jumătate de an am observat că chiar și cu picăturile acestea tot am probleme cu urechile. M-am dus la un specialist pe care-l cunoșteam, el mi-a recomandat să urmez un tratament cu Cannabis Oil. Așa și am făcut. Auzul a revenit 100%, acum mi se pare că aud chiar cum vecinii de la 2 etaje superioare respiră.",
+
+"lt107": "Maria Velișcu",
+
+"lt108": "44 de ani",
+
+"lt109": "Pe mine remediul Cannabis Oil m-a ajutat cu problemele de auz și desigur, nu e vreun remediu minune, dar își face efectul și-și face efectul bine. Mi s-a potrivit acest remediu, nu a provocat alergii, are rezultate. Per total, sunt mulțumită, pot recomanda.",
+
+"lt110": "Cristian Geambașu",
+
+"lt111": "57 de ani",
+
+"lt112": "Am cumpărat Cannabis Oil pentru tatăl meu, la bătrânețe auzul i s-a înrăutățit puternic. Cumva l-am forțat să urmeze acest tratament, mult timp s-a împotrivit. Dar acum măcar aude bine! Nu mai trebuie să-mi forțez corzile vocale când vorbesc cu el, iar când pornesc televizorul nu mai trebuie să dau volumul la maxim. Cu Cannabis Oil nu mai este nevoie să cumpărați un aparat auditiv.",
+
+"lt113": "Anghel Diaconu",
+
+"lt114": "53 de ani",
+
+"lt115": "Am cumpărat Cannabis Oil cu speranța disperată de a restabili auzul după o vătămare gravă a capului. Cu urechea stângă aproape că nu auzeam nimic, cu dreapta așa și așa. Chiar mă gândeam să-mi cumpăr un aparat auditiv, dar am decis să fac altceva – și anume să urmez un tratament cu Cannabis Oil, care mi l-a recomandat un doctor bun. Așa că tratamentul cu acest remediu m-a ajutat să-mi readuc auzul! Nu cu 100%, dar aud cu urechea stângă și dreaptă aproape la fel și foarte bine, dacă e să compar cu ce am avut înainte de acest tratament.",
+
+"lt116": "Ana Coman",
+
+"lt117": "35 de ani",
+
+"lt118": "Din copilărie am avut probleme cu urechile. Numai puțin dacă mă expun la frig, încep să mă doară urechile și-mi crește temperatura. Înainte trebuia mereu să-mi pun picături în urechi, ca să reduc din durere, de aceea m-am obișnuit ca tot timpul să aud sunete îndepărtate. Acum o jumătate de an am observat că chiar și cu picăturile acestea tot am probleme cu urechile. M-am dus la un specialist pe care-l cunoșteam, el mi-a recomandat să urmez un tratament cu Cannabis Oil. Așa și am făcut. Auzul a revenit 100%, acum mi se pare că aud chiar cum vecinii de la 2 etaje superioare respiră.",
+
+"lt119": "Maria Velișcu",
+
+"lt120": "44 de ani",
+
+"lt121": "Pe mine remediul Cannabis Oil m-a ajutat cu problemele de auz și desigur, nu e vreun remediu minune, dar își face efectul și-și face efectul bine. Mi s-a potrivit acest remediu, nu a provocat alergii, are rezultate. Per total, sunt mulțumită, pot recomanda.",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
