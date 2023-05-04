75fe9ce914a561c6f4d6a4c9bcd29fd0c1628d5f
Lenovo <ir.yastrebova@gmail.com>
2023-05-04 11:32:46 +0300

    gribok

---
+++ prokla/hu/parasits/NORMADEX/pre1/js/translater.js
@@ -0,0 +1,253 @@
+var new_lang = {
+    "lt0": "Honlap",
+
+"lt1": "Elérhetőségi linkek",
+
+"lt2": "Ugrás a tartalomhoz",
+
+"lt3": "Kisegítő lehetőségek",
+
+"lt4": "BBC Fiók",
+
+"lt5": "Értesítések",
+
+"lt6": "Keresés",
+
+"lt7": "Keresés a BBC-n",
+
+"lt8": "Keresés a BBC-n",
+
+"lt9": "Hírek",
+
+"lt10": "BBC Hírek Navigáció",
+
+"lt11": "Rovatok",
+
+"lt12": "Főoldal",
+
+"lt13": "Videók",
+
+"lt14": "Nagyvilág",
+
+"lt15": "kiválasztott",
+
+"lt16": "UK",
+
+"lt17": "Üzlet",
+
+"lt18": "Technológia",
+
+"lt19": "Tudomány",
+
+"lt20": "Történetek",
+
+"lt21": "Szórakozás &amp; Művészet",
+
+"lt22": "Egészség",
+
+"lt23": "TV hírek a nagyvilágból",
+
+"lt24": "Képekben",
+
+"lt25": "Valódiság ellenőrzése",
+
+"lt26": "Newsbeat",
+
+"lt27": "Különjelentések",
+
+"lt28": "Magyarázók",
+
+"lt29": "Riporterek",
+
+"lt30": "Mondd el",
+
+"lt31": "Nagyvilág",
+
+"lt32": "kiválasztott",
+
+"lt33": "Afrika",
+
+"lt34": "Ázsia",
+
+"lt35": "Ausztrália",
+
+"lt36": "kiválasztott",
+
+"lt37": "Európa",
+
+"lt38": "Latin-Amerika",
+
+"lt39": "Közel-Kelet",
+
+"lt40": "USA &amp; Kanada",
+
+"lt41": "A betegségek 80%-át parazitafertőzés okozza. Hogyan ne váljunk a helminták áldozatává?",
+
+"lt42": "”A parazitafertőzés számos betegség kialakulásával jár: cukorbetegség, magas vérnyomás, különböző típusú rák, meddőség és még sorolhatnánk. Ennek az az oka, hogy a paraziták végtermékei olyan toxinokat tartalmaznak, amelyek immunhiányt okoznak” – közölték az Egészségügyi Világszervezet hivatalos képviselői újságírókkal való találkozásuk során 2019. december 25-én.",
+
+"lt43": "A parazitafertőzések és a különböző betegségek közötti összefüggésről beszélt <strong>Dr. Fejér Annamária</strong>PhD, professzor, parazitológus .",
+
+"lt44": "Ma egy kissé szokatlan, de nem kevésbé fontos témáról fogunk beszélni. Nézzük a hivatalos statisztikai adatokat:",
+
+"lt45": "• Az Egészségügyi Világszervezet szerint mintegy 4,5 milliárd ember szenved különféle parazita betegségekben – ez a világ népességének több mint 60%-a",
+
+"lt46": "• Minden harmadik európai lakos érintett a parazita fertőzésben",
+
+"lt47": "• Évente 15-17 millió ember hal meg parazita fertőzésekben",
+
+"lt48": "• A helminthiasis  a harmadik leggyakoribb betegség a világon",
+
+"lt49": "Vagyis nagy valószínűséggel az ön szervezetében is vannak élősködők. De ha nem, akkor is jó eséllyel megfertőződhet.",
+
+"lt50": "Ascariasis",
+
+"lt51": "Orsóféreg",
+
+"lt52": "Ismert tény, hogy az élő szervezet nem képes túlélni szilícium nélkül. A szervezetben elszaporodó paraziták mindenekelőtt a szilíciumot kezdik el fogyasztani. Szilícium nélkül viszont a kalcium – amiből a csont felépül – nem képes tovább felszívódni. Ebből alakul ki a<strong>radikulitisz, az oszteokondrózis és a poliartritisz</strong>.",
+
+"lt53": "Trichomonas",
+
+"lt54": "A parazitológia legérdekesebb felfedezése 1989-ben történt, és “A trichomonas daganatos sejtekké való degenerációja” elnevezést adták neki. Kiderült, hogy a<strong>rák olyan betegség, amelyet paraziták okoznak</strong>. És a daganatok fő provokátora a trichomonas.",
+
+"lt55": "Így valamennyi betegség kórokozói a protozoák, gombák és helminták.",
+
+"lt56": "”A túlsúly és a helminták nem összeegyeztethetők” – gondolja ön? De ez nem igaz. A galandférgekkel fertőzött emberek esetében például  a zsír válik az egyetlen védelemmé a toxikus hatásokkal szemben.",
+
+"lt57": "Egy parazitákkal fertőzött delfin agya. Ön szerint az embernél ilyesmi nem fordulhat elő?...",
+
+"lt58": "Manapság szinte minden városban élő ember szervezetében megtalálhatók a férgek vagy a gombás fertőzések. „Ha szinte mindenkiben benne vannak, akkor ez a normális?”- kérdezi ön. Nem, nem normális!",
+
+"lt59": "Mi, parazitológusok, már régóta aggódunk emiatt. A fő probléma az, hogy hiányoznak azok az eszközök, amelyek képesek lennének megsemmisíteni minden lehetséges emberi parazitát anélkül, hogy közben kárt tennének magában az emberben.",
+
+"lt60": "Pont néhány nappal ezelőtt fejeződtek be azok a klinikai vizsgálatok, amelyeket az új<a href=\"\">Parazitol</a>nevű készítményen végeztek. Immár magabiztosan kijelenthetem, hogy ez áttörés a parazita fertőzések kezelésében. A teljes szanáció mellett az <a href=\"\">Parazitol</a>kiváló „mellékhatásokkal” rendelkezik: megtisztítja a szervezetet a salakanyagoktól és a toxinoktól, jótékony hatással van az emésztőrendszeri szervekre, stimulálja az epe kiáramlását, megszünteti a fekélyt, gyomorhurutot és sok krónikus betegséget; javítja a bőr, a haj és a köröm állapotát; normalizálja a székletet; megszünteti az allergiát.",
+
+"lt61": "Jelenleg az<a href=\"\">Parazitol</a>a<a href=\"\">gyártó hivatalos weboldalán vásárolható meg</a> alacsony áron. Siessen, mert hamarosan az<a href=\"\">Parazitol</a> bekerül a gyógyszertárakba, és teljesen más áron fogják értékesíteni.",
+
+"lt62": "Parazitol MEGRENDELÉSE",
+
+"lt63": "Megjegyzések:",
+
+"lt64": "Kati",
+
+"lt65": "gyerekek, ez borzalom... az étvágyam is elment tőle:((((((((((",
+
+"lt66": "Mónika",
+
+"lt67": "tisztaságmániás vagyok. Naponta százszor megmosom a kezemet. Folyamatosan van nálam mindenféle baktériumölő szer. Szóval remélem, hogy nekem soha nem lesz benne részem. Megyek és még egyszer kezet mosok... a biztonság kedvéért:D",
+
+"lt68": "Xénia",
+
+"lt69": "ne ijesztgessetek... GYANAKVÓ típus vagyok. ha kijön egy pattanás, már rohanok is vérvételre:))) De a gilisztákat nem észrevenni szerintem esélytelen. Belülről zabálnak fel téged... Valaminek biztosan fájnia kell. Vagy viszketni (hallottam, hogy ha gilisztáid vannak, akkor a hátsód viszket)))",
+
+"lt70": "Timi",
+
+"lt71": "akkor viszket, ha a bél alsó részén vannak, de ha fentebb vagy más szervekben telepszenek meg, akkor nem fog viszketni...",
+
+"lt72": "Orsolya",
+
+"lt73": "Sikerült akciósan megrendelni. De jó:)",
+
+"lt74": "Nóra",
+
+"lt75": "Mennyi idő alatt hozzák ki?",
+
+"lt76": "Ildikó",
+
+"lt77": "Nóra, nekem 3 nap alatt kihozták",
+
+"lt78": "Sarolta",
+
+"lt79": "Fúj... de szörnyű… nem tudom, ti hogy vagyok vele, de én megkezdem a féregirtást. Megrendeltem az Parazitol-et",
+
+"lt80": "Anna",
+
+"lt81": "Micsoda szörnyek élősködnek bennünk, szörnyű",
+
+"lt82": "Olga",
+
+"lt83": "Én régóta tudom, hogy a szervezet tele van élősködőkkel. Sokszor próbáltam csinálni úgynevezett „tisztítást”, sok féle készítményt kipróbáltam. De utána mindig nehéz volt visszaállítani a mikroflórát.",
+
+"lt84": "Írjon megjegyzést",
+
+"lt85": "Küldés",
+
+"lt86": "BBC Hírek Navigáció",
+
+"lt87": "Nagyvilág",
+
+"lt88": "Rovatok",
+
+"lt89": "BBC Hírek",
+
+"lt90": "Az ön mobilján",
+
+"lt91": "Az ön tévékészülékén",
+
+"lt92": "Értesítés a hírekről",
+
+"lt93": "BBC News Kapcsolat",
+
+"lt94": "Fedezd fel a BBC-t",
+
+"lt95": "Főoldal",
+
+"lt96": "Hírek",
+
+"lt97": "Sport",
+
+"lt98": "Időjárás",
+
+"lt99": "iPlayer",
+
+"lt100": "Hangok",
+
+"lt101": "CBBC",
+
+"lt102": "CBeebies",
+
+"lt103": "Ételek",
+
+"lt104": "Bitesize",
+
+"lt105": "Művészet",
+
+"lt106": "Kóstoló",
+
+"lt107": "Lokál",
+
+"lt108": "TV",
+
+"lt109": "Rádió",
+
+"lt110": "Három",
+
+"lt111": "Használati feltételek",
+
+"lt112": "A BBC-ről",
+
+"lt113": "Adatvédelmi irányelvek",
+
+"lt114": "Sütik",
+
+"lt115": "Kisegítő lehetőségek",
+
+"lt116": "Szülői útmutatás",
+
+"lt117": "Vegye fel a kapcsolatot a BBC-vel",
+
+"lt118": "Személyre szabott hírlevél igénylése",
+
+"lt119": "<em class=\"orb-hilight\">Copyright © 2019 BBC.</em>A BBC nem vállal felelősséget a külső webhelyeken megjelenő tartalomért.<span class=\"orb-footer-links\"> <a class=\"orb-hilight\" href=\"\">Olvasson a külső megközelítésünk linkeléséről.</a> </span>"
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
---
+++ prokla/hu/parasits/NORMADEX/pre2/js/translater.js
@@ -0,0 +1,217 @@
+var new_lang = {
+    "lt0": "LiveJournal",
+
+"lt1": "Fő",
+
+"lt2": "Értékelések",
+
+"lt3": "Belépés",
+
+"lt4": "BLOG LÉTREHOZÁSA",
+
+"lt5": "Politika és társadalom",
+
+"lt6": "A világ körül",
+
+"lt7": "Mozi",
+
+"lt8": "Tudomány és technológia",
+
+"lt9": "Szórakozás",
+
+"lt10": "Család és gyerekek",
+
+"lt11": "Ételek",
+
+"lt12": "Szépség és egészség",
+
+"lt13": "Életmód",
+
+"lt14": "20 éves a LJ",
+
+"lt15": "Több",
+
+"lt16": "Főoldalra",
+
+"lt17": "Big_Master",
+
+"lt18": "10 perc olvasásra",
+
+"lt19": "A bejegyzés 156. helyen áll a rangsorban",
+
+"lt20": "LJ ajánlja",
+
+"lt25": "Kategória:",
+
+"lt26": "Egészség",
+
+"lt27": "Mégsem",
+
+"lt28": "Hogyan tették majdnem tönkre a paraziták 7 nap alatt az életemet",
+
+"lt29": "_25E2_2580_259DElvesztettem a munk_25C3_25A1mat _25C3_25A9s majdnem az eg_25C3_25A9szs_25C3_25A9gem 0D128CD4BD",
+
+"lt30": "A nővérem időnként nálam hagyta a három éves unokaöcsémet, és egyik alkalommal elmondta, hogy férgességet állapítottak meg nála. Egyből arra gondoltam, hogy talán én is megfertőződtem és rendeltem az interneten parazitaölő teát.",
+
+"lt31": "Mire megjött a rendelés (3 hét alatt postán), viszketést éreztem a végbélnyílásomnál. Útmutatás szerint szedtem. Ez alatt a viszketés tovább erősödött, főleg éjszakánként, alig aludtam. Anyukám tanácsára egy másik készítményt kezdtem el szedni abban a reményben, hogy az majd segít. Negyedik napra hasmenésem lett, néhány napig tartott. A viszketés nem szűnt meg, sőt erősebb lett, és olyannyira elviselhetetlenné vált, hogy nem tudtam aludni. Pánikba estem, foltok jelentek meg a szemem alatt. A kezem kiütéses lett, fehér pöttyök jöttek ki, amelyeket kipukkasztottam, allergia lehetett:",
+
+"lt32": "Ilyen állapotban nem folytathattam a munkámat, rémesen éreztem magam. Nem vehettem ki szabadságot. Érthető, hogy a valódi okáról nem beszélhettem. El kellett jönnöm.",
+
+"lt33": "Továbbra is szedtem a gyógyszert, remélve, hogy ez a legrosszabb és majd meggyógyulok.",
+
+"lt34": "Megfájdult a gyomrom, a beleim. A viszketés továbbra sem múlt el. Az ánuszom környékén kisebesedett. Annyira szenvedtem, hogy éjszakánként még az öngyilkosság gondolata is megkörnyékezett. Nem tudtam, mit tegyek, az állapotom egyre csak romlott. Végigszedtem a gyógyszert, de semi javulás.",
+
+"lt35": "Nem akartam ölbe tett kézzel ülni, orvosi segítséget kerestem. Másfél héttel később találtam orvost az Higiéniai és Epidemiológiai Központban, 3 labort csináltattam parazitákra, székletmintát és kenetet enterobiasisra. Egy csomó pénzem elment a konzultációra és a vizsgálatokra.",
+
+"lt36": "A kenet alapján végbélgilisztát állapítottak meg. Az orvos az<a href=\"\">Parazitol-et</a>írta fel, mivel problémáink voltak az emésztőrendszerrel, kímélő kezelésre volt szükségem. Napi egy kapszulát kell bevenni. Az első használat után másnapra kijöttek belőlem a kicsi fehér paraziták. Egyből jobban lettem.",
+
+"lt37": "Egy hét elteltével újból kivizsgáltattam magam és az eredmény negatív lett! De az orvos azt tanácsolta, hogy továbbra is szedjem az<a href=\"\">Parazitol-et</a>, mivel helyreállítja a gyomor és a bél mikroflóráját, megtisztítja az epevezetékeket és helyreállítja az emésztőrendszer működését.",
+
+"lt38": "Az <a href=\"\">Parazitol-el</a>végzett kúra után tényleg sokkal jobban kezdtem el magam érezni, mint a betegség előtt! Javult a hajam, a körmöm és a bőröm állapota. Sok krónikus problémám megszűnt.",
+
+"lt39": "Minden betegséget a paraziták okoznak  – ez igaz.",
+
+"lt40": "Egy egész hónapot elfecséreltem olyan gyógyszerek szedésére, amelyek nem használtak, szenvedtem a betegség elviselhetetlen tüneteitől, és az egész majdnem tragédiába torkollott.",
+
+"lt41": "Most munkát keresek.",
+
+"lt42": "Az orvos azt ajánlotta, hogy az<a href=\"\">Parazitol</a>segítségével fél évente egyszer végezzem el a terápiás tisztítást a parazitáktól. Emellett jó, ha az egész család ezt megteszi, különben az egésznek semmi értelme: folyamatos fertőzéseknek leszel kitéve.",
+
+"lt43": "Egyébként megosztom a<a href=\"\">weboldal linkjét</a>, ahol megrendelhetitek az<a href=\"\">Parazitol-et</a>vételáron (gyógyszertárban sokkal többe kerül). Amúgy is munkanélküli vagyok, úgyhogy nekem ez sokat számít.",
+
+"lt44": "Parazitol megrendelése árrések nélkül a gyártótól",
+
+"lt49": "Big_Master",
+
+"lt50": "Hozzáadás ismerősökhöz",
+
+"lt51": "Még több LJ MEDIA",
+
+"lt52": "a_nalgin <span class=\"itemTimereading\">Olvasás 5 perc.</span>",
+
+"lt53": "A fiatalság nagy hibájáról, amelyet elkövet minden harmadik...",
+
+"lt54": "bookmatejournal <span class=\"itemTimereading\">Olvasás 9 perc.</span>",
+
+"lt55": "Nikolai Fomenko: „Tolsztoj és Puskin szép lassan...",
+
+"lt56": "fomaru <span class=\"itemTimereading\">Olvasás 10 perc.</span>",
+
+"lt57": "”Kis kanállal etettem őket, beraktam nekik a kacsát, de...",
+
+"lt58": "gelio <span class=\"itemTimereading\">Olvasás 19 perc.</span>",
+
+"lt59": "Taskent madártávlatból: Közép-Ázsia legnagyobb városa...",
+
+"lt60": "Weekly Newsletter",
+
+"lt61": "Iratkozzon fel független híroldalunkra",
+
+"lt62": "a LiveJournal legjobb bloggereitől",
+
+"lt63": "Feliratkozás",
+
+"lt64": "Megjegyzések:",
+
+"lt65": "Anna",
+
+"lt66": "Szörnyű! Azonnal meg kellett volna kezdeni a helyes kezelést",
+
+"lt67": "Válasz",
+
+"lt68": "Elrejtés",
+
+"lt69": "Erzsébet",
+
+"lt70": "Ami a megelőző tisztítást illeti, nekem biztosan nem ártana. Szeretem a macskákat, folyton ölelgetem, puszilgatom őket, lehet, hogy már valamit el is kaptam tőlük.",
+
+"lt71": "Válasz",
+
+"lt72": "Elrejtés",
+
+"lt73": "Imre",
+
+"lt74": "Rákerestem az Parazitol-re az interneten és azt írják, hogy megszünteti a fekélyeket és a gyomorhurutot, mivel megöli azokat a baktériumokat, amelyek kiváltják. Érdekelni kezdett. Én is megrendelem",
+
+"lt75": "Válasz",
+
+"lt76": "Elrejtés",
+
+"lt77": "Katalin",
+
+"lt78": "Sikerült akciósan megrendelni. De jó! 😊",
+
+"lt79": "Válasz",
+
+"lt80": "Elrejtés",
+
+"lt81": "Anett",
+
+"lt82": "Tavaly Ázsiába utaztunk, mondhatni gasztronómiai körutazás volt. Hazaérkezvén az egész család nem volt jól. Sejthetitek, miért. Végigszedtük az Parazitol-et és már az első napokban éreztük a javulást. Most már mi is, csak úgy, mint a szerző, megelőzés céljából szedjük.",
+
+"lt83": "Válasz",
+
+"lt84": "Elrejtés",
+
+"lt85": "Klára",
+
+"lt86": "Amióta az Parazitol-et szedem, megszűnt az allergiám!",
+
+"lt87": "Válasz",
+
+"lt88": "Elrejtés",
+
+"lt89": "Mihály",
+
+"lt90": "Már egy hónapja szedem az Parazitol-et. Sokkal jobban érzem magam, erősebbnek, több az energiám, mintha 10 évet fiatalodtam volna. 72 éves vagyok",
+
+"lt91": "Válasz",
+
+"lt92": "Elrejtés",
+
+"lt93": "Jolán",
+
+"lt94": "Mennyi idő alatt szállítják ki?",
+
+"lt95": "Válasz",
+
+"lt96": "Elrejtés",
+
+"lt97": "Ilona",
+
+"lt98": "Jolán, nekem 3 nap alatt kihozták",
+
+"lt99": "Válasz",
+
+"lt100": "Elrejtés",
+
+"lt101": "Olga",
+
+"lt102": "A múlt hónapban az Parazitol-et szedtem. A családban mindenkinek javult a közérzete. Megszűnt a krónikus mandulagyulladásom  (azt hittem, már soha nem szabadulok meg tőle). A férjemnek az Parazitol enyhítette a súlyos pikkelysömörét. Még egyszer végig kéne szedni mindenkinek. Egyébként gyógyszertárban drágább. Ezúttal interneten rendelem meg",
+
+"lt103": "Válasz",
+
+"lt104": "Elrejtés",
+
+"lt105": "Megjegyzés hozzáadása",
+
+"lt106": "Rólunk",
+
+"lt107": "Felhasználói megállapodás",
+
+"lt108": "Segítség",
+
+"lt109": "\"Megosztás\"gomb"
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
